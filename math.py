04ee3f994e2a4a96759344b4e069ae6e82cccdaa
jay <jsaswar7@gmail.com>
2021-06-03 14:45:52 +0530

    Add fn headers

--- math.py
+++ math.py
@@ -1,12 +1,16 @@
+#Add Implementation
 def add(x,y):
 	return x+y
-	
+
+#Subtract Implementation	
 def subtract(x,y):
 	pass
-		
+	
+#Multiply Implementation	
 def multiply(x,y):
 	pass
 	
+#Divide Implementation	
 def divide(x,y):
 	pass
 	
